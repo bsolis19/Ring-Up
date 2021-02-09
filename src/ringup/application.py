e725c5fc777d8d7a8cc8f636c35e0f2fd06b2c78
bsolis19 <benjairsolis19@gmail.com>
2021-02-09 15:31:07 -0800

    remove __name__ variable check

--- src/ringup/application.py
+++ src/ringup/application.py
@@ -11,4 +11,12 @@ class Application(tk.Tk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title(s.APP_NAME)
-        self.resizable(width=False, height=False)
+        #self.resizable(width=False, height=False)
+        ttk.Label(
+                self,
+                text=s.APP_NAME,
+                font=('tkDefaultFont', 16),
+                ).grid(row=0)
+        self.productform = v.ProductForm(self)
+        self.productform.grid(row=1, padx=10)
+
