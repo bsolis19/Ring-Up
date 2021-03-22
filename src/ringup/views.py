00820bb5d36ebcfe84b57848d2d0b333456c6d8c
bsolis19 <benjairsolis19@gmail.com>
2021-03-22 14:34:35 -0700

    add details table to product form

--- src/ringup/views.py
+++ src/ringup/views.py
@@ -23,6 +23,7 @@ class ProductForm(Form):
 
         # Data
         self.model = model
+        self.model.details = {'color': 'red', 'foo': 'bar'}
         self.header_var = tk.StringVar(value=self.model.title)
 
         # Containers
@@ -104,8 +105,8 @@ class ProductForm(Form):
 
     def _build_details_frame(self, parent):
         container = tk.Frame(parent, background='red')
-        lb = tk.Listbox(container, background='blue')
-        lb.pack()
+        table_component = self._build_entries_table(container)
+        table_component.pack(fill=tk.X)
         return container
 
     def _build_description_frame(self, parent):
@@ -113,6 +114,17 @@ class ProductForm(Form):
         container.pack_propagate(False)
         textbox = tk.Text(container, background='blue')
         textbox.pack()
+        return container
+
+    def _build_entries_table(self, parent):
+        container = tk.Frame(parent)
+        header1, header2 = self._build_pair(container, 'Detail', 'Value')
+        header1.grid()
+        header2.grid(row=0, column=1)
+        container.columnconfigure(0, weight=1)
+        container.columnconfigure(1, weight=1)
+        self.load_details(container)
+        self.append_empty_detail_entry(container)
         return container
 
     def _build_price_output(self, parent):
@@ -131,3 +143,53 @@ class ProductForm(Form):
                 )
         self.output.grid(row=1, column=0)
         return container
+
+    def _build_pair(self, parent, txt1='', txt2='', class_=tk.Label):
+        if class_ == tk.Label:
+            w1 = class_(parent, text=txt1)
+            w2 = class_(parent, text=txt2)
+        elif class_ == tk.Entry:
+            w1 = class_(parent)
+            w1.insert(0, txt1)
+            w2 = class_(parent)
+            w2.insert(0, txt2)
+        else:
+            raise ValueError('Widget pair could not be created')
+        return w1,w2
+
+    def load_details(self, container):
+        details = self._get_details()
+        self._display_details(container, details)
+
+    def _get_details(self):
+        return self.model.details
+
+    def _display_details(self, parent, details):
+        # row 0 used by header text
+        current_row = 1
+        current_col = 0
+        for detail in details:
+            detail_entry = tk.Entry(parent)
+            value_entry = tk.Entry(parent)
+            detail_entry.insert(0, str(detail))
+            value_entry.insert(0, str(details[detail]))
+
+            detail_entry.grid(
+                    row=current_row,
+                    column=current_col,
+                )
+            current_col += 1
+            value_entry.grid(
+                    row=current_row,
+                    column=current_col,
+                )
+            current_row += 1
+            current_col -= 1
+
+    def append_empty_detail_entry(self, parent):
+        # +1 due to header text occupying first row
+        row = len(self._get_details()) + 1
+        empty1, empty2 = self._build_pair(parent=parent, class_=tk.Entry)
+        empty1.grid(row=row, column=0)
+        empty2.grid(row=row, column=1)
+
